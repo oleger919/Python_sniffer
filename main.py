d9207a40d5796216e30ed3ec50f007fdb5d39087
oleger919 <60582617+oleger919@users.noreply.github.com>
2022-03-15 23:29:05 +0300

    Второе окно, доработки первого

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import scapy.all as scapy
-import gui
+import mainwindow
+import window2
 import sys
 import psutil
 
-app = gui.QtWidgets.QApplication(sys.argv)
-MainWindow = gui.QtWidgets.QMainWindow()
-ui = gui.Ui_MainWindow()
+# import darktheme
+# from darktheme.widget_template import DarkPalette
+# app.setPalette(DarkPalette())
+
+app = mainwindow.QtWidgets.QApplication(sys.argv)
+MainWindow = mainwindow.QtWidgets.QMainWindow()
+ui = mainwindow.Ui_mainWindow()
 ui.setupUi(MainWindow)
+
+window2 = window2.QtWidgets.QDialog()
+ui2 = window2.Ui_Dialog()
+ui2.setupUi(window2)
 
 
 class Sniff:
@@ -18,7 +27,7 @@ class Sniff:
         print(packet)
 
     def sniff(self, interface):
-        scapy.sniff(iface=interface, store=False, prn=self.process_sniffed, filter="tcp")
+        scapy.sniff(iface=interface, store=False, prn=self.process_sniffed, filter="ip")
 
     # sniff('Беспроводная сеть')
 
@@ -31,7 +40,12 @@ class DecorateGui:
         self.check_net_addrs()
 
     def btn1_click(self):
-        print('нажатие btn1')
+        sniffer = Sniff()
+        # print('нажатие btn1')
+        # Sniff.sniff(sniffer, 'Беспроводная сеть')
+        MainWindow.hide()
+        window2.show()
+        sys.exit(app.exec_())
 
     def cmbbox_2_onchange(self):
         # Отслеживание изменения ComboBox_2 и TODO активация ввода параметра вручную
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-import psutil
-
-addrs = psutil.net_if_addrs()
-print(addrs.keys())
+# import psutil
+#
+# addrs = psutil.net_if_addrs()
+# print(addrs.keys())
 #
 # if __name__ == "__main__":
 #     app = QtWidgets.QApplication(sys.argv)
@@ -10,3 +10,19 @@ print(addrs.keys())
 #     ui.setupUi(MainWindow)
 #     MainWindow.show()
 #     sys.exit(app.exec_())
+import scapy.all as scapy
+
+
+class Sniff:
+
+    def __init__(self):
+        _bpf_filter_args = []
+
+    def process_sniffed(self, packet):
+        print(packet)
+
+    def sniff(self, interface):
+        scapy.sniff(iface=interface, store=False, prn=self.process_sniffed, filter="ip")
+
+sn = Sniff()
+Sniff.sniff(sn, 'Беспроводная сеть')
