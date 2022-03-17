b91ac952221df3199b1638e961ee369d9c5d4b00
oleger919 <60582617+oleger919@users.noreply.github.com>
2022-03-17 22:45:33 +0300

    Корректное открытие второго окна и приведение кода в порядок

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import scapy.all as scapy
-import mainwindow
-import window2
+import mainwindownew as mainwindow
+import window2 as second_window
 import sys
 import psutil
+from PyQt5 import QtWidgets
 
 # import darktheme
 # from darktheme.widget_template import DarkPalette
 # app.setPalette(DarkPalette())
-
-app = mainwindow.QtWidgets.QApplication(sys.argv)
-MainWindow = mainwindow.QtWidgets.QMainWindow()
-ui = mainwindow.Ui_mainWindow()
-ui.setupUi(MainWindow)
-
-window2 = window2.QtWidgets.QDialog()
-ui2 = window2.Ui_Dialog()
-ui2.setupUi(window2)
 
 
 class Sniff:
@@ -32,34 +24,52 @@ class Sniff:
     # sniff('Беспроводная сеть')
 
 
-class DecorateGui:
+class Window_2(QtWidgets.QWidget, second_window.Ui_Dialog):
+    def __init__(self, parent=None):
+        super(Window_2, self).__init__(parent)
+        self.setupUi(self)
 
-    def __init__(self):
-        ui.pushButton.clicked.connect(DecorateGui.btn1_click)
-        ui.comboBox_2.currentTextChanged.connect(DecorateGui.cmbbox_2_onchange)
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        uic = mainwindow.Ui_mainWindow()
+        uic.setupUi(self)
+        self.ui = uic
+        uic.pushButton.clicked.connect(self.show_window_2)
+        uic.comboBox_2.currentTextChanged.connect(Main.cmbbox_2_onchange)
+        uic.lineEdit.hide()
         self.check_net_addrs()
 
-    def btn1_click(self):
+    def show_window_2(self):  # открытие 2  окна
+        self.inst = Window_2()
+        self.inst.show()
+
         sniffer = Sniff()
-        # print('нажатие btn1')
-        # Sniff.sniff(sniffer, 'Беспроводная сеть')
-        MainWindow.hide()
-        window2.show()
+        interface = self.ui.comboBox_1.currentText()
+        print(interface)
+        Sniff.sniff(sniffer, interface)
         sys.exit(app.exec_())
 
     def cmbbox_2_onchange(self):
+        pass
         # Отслеживание изменения ComboBox_2 и TODO активация ввода параметра вручную
-        if ui.comboBox_2.currentText() == 'Ввести вручную':
-            pass
+        # print('запуск')
+        # print(str(self.comboBox_2.currentText()))
+        # if self.ui.comboBox_2.currentText() == 'Ввести вручную':
+        #     self.ui.lineEdit.show()
+
+
 
     def check_net_addrs(self):
         # Просмотр доступных сетевых интерфейсов и добавление списка в ComboBox_1
         addrs = psutil.net_if_addrs()
         # print(addrs.keys())
-        ui.comboBox_1.addItems(addrs)
+        self.ui.comboBox_1.addItems(addrs)
 
 
 if __name__ == "__main__":
-    Start_changes = DecorateGui()
-    MainWindow.show()
+    app = QtWidgets.QApplication(sys.argv)
+    w = Main()
+    w.show()
     sys.exit(app.exec_())
