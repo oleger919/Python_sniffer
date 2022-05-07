520d874e8830bceb2690d17b74283c87f9a1f5d7
oleger919 <60582617+oleger919@users.noreply.github.com>
2022-05-07 19:51:29 +0300

    конец дня 07.05

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 import platform
+import time
 import traceback
 import scapy.all as scapy
 from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem
 from scapy.layers.inet import IP
 from scapy.layers.http import Raw
-import mainwindow as mainwindow
-import second_window_bar as second_window
 import sys
 import psutil
 from PyQt5 import QtWidgets, QtCore
 from scapy.utils import hexdump, PcapReader
 from shutil import move as move_file
+from shutil import copyfile
 from platform import system
+import mainwindow as mainwindow
+import second_window_bar as second_window
+import wait as wait_msg
 
 # import darktheme
 # from darktheme.widget_template import DarkPalette
@@ -48,6 +51,8 @@ class SniffThread(QThread):
         self.my_sniffer = Sniff()
 
     def run(self):
+        """ parameter "mode" : 0 - reading file, 1 - real-time sniff"""
+        # print('mode in sniffthread = ', self.mode)
         while self.running:
             self.my_sniffer.sniff()
 
@@ -60,6 +65,10 @@ class Sniff:
         self.filter_to_sniff = filter_to_sniff
         self.processing = True
         self.packet_list = []
+        # self.please_wait = WaitWindow()
+        # self.please_wait.show()
+        # self.please_wait.hide()
+
 
     def process_sniffed(self, packet):
         global root_window
@@ -80,8 +89,8 @@ class Sniff:
                     add_to_table_list['protocol'] = str(packet.proto)
                 if packet.haslayer(Raw):
                     add_to_table_list['raw'] = str(packet[Raw].load)
-                print(add_to_table_list)
-                print(packet.show())
+                # print(add_to_table_list)
+                # print(packet.show())
                 # print(hexdump(packet))
 
                 rows_count = root_window.inst.ui.snifftable.rowCount()
@@ -99,27 +108,43 @@ class Sniff:
                 # self.packet_list.append(packet)
                 scapy.wrpcap('sniffed', packet, append=True)
 
-    def sniff(self):
+    def sniff(self, mode=1):
+        """ parameter "mode" : 0 - reading file, 1 - real-time sniff"""
         global iface_to_sniff, filter_to_sniff
         self.iface_to_sniff = iface_to_sniff
         self.filter_to_sniff = filter_to_sniff
-        while self.processing:
-            scapy.sniff(count=10, iface=self.iface_to_sniff, filter=self.filter_to_sniff, store=False,
-                        prn=self.process_sniffed)
+        # print(mode)
+        if mode == 1:
+            while self.processing:
+                scapy.sniff(count=10, iface=self.iface_to_sniff, filter=self.filter_to_sniff, store=False,
+                            prn=self.process_sniffed)
+        elif mode == 0:
+            pcap_file = QtWidgets.QFileDialog.getOpenFileName()
+            scapy.sniff(offline=pcap_file[0], prn=self.process_sniffed, store=0)
+            QMessageBox.information(None, 'Анализатор трафика', 'Загрузка из файла прошла успешно')
+        else:
+            QMessageBox.critical(None, 'Ошибка', 'Передано неверное значение параметра mode')
 
     def stop_sniff(self):
         self.processing = False
-        # scapy.wrpcap(filename='sniffed', pkt=self.packet_list)
 
     def read_from_pcap(self):
         pcap_file = QtWidgets.QFileDialog.getOpenFileName()
-        print(pcap_file)
+        # print(pcap_file)
         # for packet in PcapReader(pcap_path):
         #     try:
         #         if packet[TCP].dport == 80:
         #
         #     except Exception as e:
         #         pass
+
+
+class WaitWindow(QtWidgets.QDialog, wait_msg.Ui_Dialog):
+    def __init__(self, parent=None):
+        super(WaitWindow, self).__init__(parent)
+        uic = wait_msg.Ui_Dialog()
+        self.ui = uic
+        uic.setupUi(self)
 
 
 class Window_2(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
@@ -131,33 +156,77 @@ class Window_2(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
         uic.start_button.clicked.connect(self.start_sniff)
         uic.stop_button.clicked.connect(self.stop_sniff)
         self.cols = uic.snifftable.columnCount()
-        # uic.snifftable.hideColumn(5)
 
-        # self.ui.menubar = QtWidgets.QMenuBar()
-        # fileMenu = self.ui.menubar.addMenu('&File')
+        uic.open_file_action.triggered.connect(self.open_pcap_file)
+        uic.save_file_action.triggered.connect(self.save_pcap_file)
+        uic.show_info_action.triggered.connect(self.show_info)
+        uic.clear_table_action.triggered.connect(self.clear_snifftable)
+
+        uic.snifftable.hideColumn(5)
+        uic.snifftable.hideColumn(6)
+        uic.snifftable.hideColumn(7)
 
         self.second_thread = SniffThread()
 
         self.mouse_press = None
         uic.snifftable.cellPressed[int, int].connect(self.clickedRowColumn)
+
         # uic.snifftable.hideColumn(0)
 
-    def clickedRowColumn(self, r, c):
-        row = self.ui.snifftable.currentItem().row()
-        item = (self.ui.snifftable.item(row, 5)).text()
-        hexitem = hexdump(item, dump=True)
-        self.ui.hexdump_edit.setText(hexitem)
+    def show_info(self):
+        QMessageBox.information(None, 'О программе', 'Данная программа представляет собой '
+                                                     'пассивный сниффер для анализа '
+                                                     'трафика в локальной сети \n'
+                                                     'Разработчик: Олег Баранов')
 
-    def start_sniff(self):
+    def clear_snifftable(self):
+        self.ui.snifftable.setRowCount(0)
+        self.ui.snifftable.setRowCount(1)
+
+    def clickedRowColumn(self, r, c):
+        if self.ui.snifftable.currentItem() is not None:
+            row = self.ui.snifftable.currentItem().row()
+            item = (self.ui.snifftable.item(row, 5)).text()
+            hexitem = hexdump(item, dump=True)
+            self.ui.hexdump_edit.setText(hexitem)
+
+    def open_pcap_file(self):
+        # print('ща откроем')
+        self.stop_sniff()
+        self.ui.snifftable.setRowCount(0)
+        self.ui.snifftable.setRowCount(1)
+        # self.start_sniff(mode=0)
+        self.my_sniffer = Sniff()
+        # self.please_wait = WaitWindow()
+        # self.please_wait.show()
+        # self.please_wait.hide()
+        self.my_sniffer.sniff(mode=0)
+
+    def save_pcap_file(self):
+        fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Open file', '/packets', 'PCAP файл (*.pcapng)')[0]
+        filename = ''
+        # print(platform.system())
+        if platform.system() == 'Windows':
+            filename = '\sniffed'
+        elif platform.system() == 'Linux' or 'Darwin':
+            filename = '/sniffed'
+        current_dir = os.path.dirname(os.path.abspath(__file__)) + filename
+        copyfile(current_dir, fname)
+
+    def start_sniff(self, mode=1):
+        """ parameter "mode" : 0 - reading file, 1 - real-time sniff"""
         global iface_to_sniff, filter_to_sniff
+        self.ui.hexdump_edit.setText('Нажмите на строку, чтобы увидеть шестнадцатеричное представление пакета')
         self.ui.what_iface_sniff.setText(iface_to_sniff)
         if not self.second_thread.running:
             self.second_thread.running = True
             self.second_thread.my_sniffer.processing = True
         self.second_thread.start()
+        # print('mode in start_sniff = ', mode)
+        # self.second_thread.run(mode=1)
 
     def stop_sniff(self):
-        print('нажатие на стоп')
+        # print('нажатие на стоп')
         self.second_thread.running = False
         self.second_thread.my_sniffer.stop_sniff()
 
@@ -171,7 +240,7 @@ class Window_2(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
         if reply == QMessageBox.Yes:
             fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Open file', '/packets', 'PCAP файл (*.pcapng)')[0]
             filename = ''
-            print(platform.system())
+            # print(platform.system())
             if platform.system() == 'Windows':
                 filename = '\sniffed'
             elif platform.system() == 'Linux' or 'Darwin':
